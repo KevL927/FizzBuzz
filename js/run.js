77f5f0467f241ff1ddea9823d17bb69bc7393f0f
KevL927 <leekevin@gmail.com>
2016-08-10 18:06:40 -0400

    Final Version of FizzBuzz

--- js/run.js
+++ js/run.js
@@ -1,14 +1,14 @@
 function fizzBuzz () {
 var finalAnswer = "";
   for(var i = 1; i <= 100; i++){
-    if(i % 3 === 0 && i % 5 === 0){
-      finalAnswer += "FizzBuzz\n";
-    }else if(i % 3 === 0){
-      finalAnswer += "Fizz\n";
-    }else if(i % 5 === 0){
-      finalAnswer += "Buzz\n";
-    }else{
-      finalAnswer += i + "\n";
+    if (i % 3 === 0 && i % 5 === 0){
+      finalAnswer += "FizzBuzz<br>";
+    } else if (i % 3 === 0){
+      finalAnswer += "Fizz<br>";
+    } else if (i % 5 === 0){
+      finalAnswer += "Buzz<br>";
+    } else {
+      finalAnswer += i + "<br>";
     }
   }
   return finalAnswer;
